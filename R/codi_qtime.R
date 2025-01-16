0d3ec1f358363f39e103552480f100f6d7f132a3
mesteban <mesteban@bsc.es>
2025-01-16 20:59:41 +0100

    Added residuals analysis

--- R/codi_qtime.R
+++ R/codi_qtime.R
@@ -1,13 +1,14 @@
 library(mixtools)
 library(ggplot2)
 library(gridExtra)
+library(hrbrthemes)
+library(ggpubr)
 
 # Load custom negative log-likelihood function
 source("R/llh_covars_qtime.R")
 
 # Load main data
 load("R/data_main.RData")
-# load("Data/max_llh.RData")
 
 # Covariates matrix (Time, Age, Sex, Interaction, Sine, Cosine)
 t <- rep(seq(1, 96), 4) / 96
@@ -147,9 +148,21 @@ reconstruct_incid <- function(incid, post, q_time_func) {
 #' @param ylim_range Numeric vector defining the y-axis range for the plot.
 #' @param main_title Character string representing the title of the plot.
 plot_time_series <- function(incid, xrec, start_year, end_year, ylim_range, main_title) {
-  ts.plot(ts(incid, start = c(start_year, 1), end = c(end_year, 12), freq = 12), ylim = ylim_range, ylab = "Incidence x 100,000", main = main_title)
-  lines(seq(start_year, end_year + 0.99, 1 / 12), xrec, col = "red", lty = 2)
-  legend("topright", legend = c("Observed", "Reconstructed"), col = c("black", "red"), lty = c(1, 2))
+  # ts.plot(ts(incid, start = c(start_year, 1), end = c(end_year, 12), freq = 12), ylim = ylim_range, ylab = "Incidence x 100,000", main = main_title)
+  # lines(seq(start_year, end_year + 0.99, 1 / 12), xrec, col = "red", lty = 2)
+  # legend("topright", legend = c("Observed", "Reconstructed"), col = c("black", "red"), lty = c(1, 2))
+  
+  p <- ggplot(data = data.frame(incid = incid, xrec = xrec, time = seq(start_year, end_year + 0.99, 1 / 12))) +
+    geom_line(aes(x = time, y = incid), color = "black") +
+    geom_line(aes(x = time, y = xrec), color = "red", linetype = "dashed") +
+    labs(x = "Year", y = "Incidence x 100,000", title = main_title) +
+    scale_x_continuous(breaks = seq(start_year, end_year, 1), 
+                       labels = scales::label_number(accuracy = 1)) +
+    theme_ipsum_ps()
+
+  show(p)
+  
+  return(p)
 }
 
 # Process and analyze incidence data for a specific demographic group
@@ -165,7 +178,7 @@ process_demographic_group <- function(data, covariates, start_year, end_year, yl
   cat("Posterior probabilities calculated\n")
   xrec <- reconstruct_incid(data$incid, post, q_time)
   cat("Incidence reconstructed\n")
-  plot_time_series(data$incid, xrec, start_year, end_year, ylim_range, main_title)
+  p1 <- plot_time_series(data$incid, xrec, start_year, end_year, ylim_range, main_title)
   cat("Time series plotted\n")
   
   # Load the paper result
@@ -174,10 +187,108 @@ process_demographic_group <- function(data, covariates, start_year, end_year, yl
 
   # Plot the percentage difference
   percentage_diff <- (xrec_paper - xrec) / xrec_paper * 100
-  p <- ts.plot(ts(percentage_diff, start = c(start_year, 1), end = c(end_year, 12), freq = 12), ylim = c(-5, 5), ylab = "Percentage Difference", main = paste0("Percentage Difference: ", main_title))
+
+  # ts.plot(ts(percentage_diff, start = c(start_year, 1), end = c(end_year, 12), freq = 12), ylim = c(-5, 5), ylab = "Percentage Difference", main = paste0("Percentage Difference: ", main_title))
+  
+  p2 <- ggplot(data = data.frame(percentage_diff = percentage_diff, time = seq(start_year, end_year + 0.99, 1 / 12))) +
+    geom_line(aes(x = time, y = percentage_diff)) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    labs(x = "Year", y = "Percentage Difference", title = main_title) +
+    scale_x_continuous(breaks = seq(start_year, end_year, 1), 
+                       labels = scales::label_number(accuracy = 1)) +
+    theme_ipsum_ps()
+  show(p2)
+  
+  return(p1)
 }
 
-process_demographic_group(pr3[pr3$sexe == 0 & pr3$age == 0, ], covars[covars[, 2] == 0 & covars[, 3] == 0, ], 2009, 2016, c(9, 32), "Women 15-29 years old")
-process_demographic_group(pr3[pr3$sexe == 0 & pr3$age == 1, ], covars[covars[, 2] == 1 & covars[, 3] == 0, ], 2009, 2016, c(1, 8), "Women 30-94 years old")
-process_demographic_group(pr3[pr3$sexe == 1 & pr3$age == 0, ], covars[covars[, 2] == 0 & covars[, 3] == 1, ], 2009, 2016, c(4, 32), "Men 15-29 years old")
-process_demographic_group(pr3[pr3$sexe == 1 & pr3$age == 1, ], covars[covars[, 2] == 1 & covars[, 3] == 1, ], 2009, 2016, c(1, 12), "Men 30-94 years old")
+#' Compute global residuals for the model
+#' @param max_llh A list containing the maximum likelihood estimates for the parameters.
+#' @param pr3 Data frame containing the incidence data.
+#' @param covars Matrix of covariates for the dataset.
+#' @param q_time Function representing the dynamic underreporting factor.
+perform_residual_analysis <- function(max_llh, pr3, covars, q_time) {
+  # Initialize vectors
+  y_est <- vector()
+  w     <- vector()
+  
+  # Loop to calculate w and y_est
+  for (i in 1:384) {
+    j <- (i %% 96) / 96
+    if (j == 0) j <- 1
+    
+    w[i] <- exp(max_llh$estimate[1] + max_llh$estimate[2] * j) /
+      (1 + exp(max_llh$estimate[1] + max_llh$estimate[2] * j))
+    
+    m <- max_llh$estimate[3] + max_llh$estimate[4] * j + 
+      max_llh$estimate[5] * pr3$age[i] +
+      max_llh$estimate[6] * pr3$sexe[i] +
+      max_llh$estimate[7] * pr3$inter[i] +
+      max_llh$estimate[8] * covars[i, 5] +
+      max_llh$estimate[9] * covars[i, 6]
+    
+    y_est[i] <- w[i] * m + (1 - w[i]) * m / q_time(j)
+  }
+  
+  # Aggregate y_est
+  y_est_agg_temp <- data.frame(
+    t = rep(seq(1:96), 4),
+    sexe = c(rep(0, 192), rep(1, 192)),
+    edat = c(rep(0, 96), rep(1, 96), rep(0, 96), rep(1, 96)),
+    y_est
+  )
+  
+  tw <- sum(unique(pr3$Pob)) 
+  y_est_agg <- aggregate(y_est_agg_temp$y_est, by = list(y_est_agg_temp$t), FUN = sum)
+  y_agg <- aggregate(pr3$incid, by = list(pr3$mes_any_problema), FUN = sum)
+  
+  # Normalize y_est_agg and y_agg by year
+  for (year in 2009:2016) {
+    indices <- ((year - 2009) * 12 + 1):((year - 2008) * 12)
+    norm_factor <- sum(unique(pr3$Pob[pr3$Year == year])) / tw
+    y_est_agg$x[indices] <- y_est_agg$x[indices] * norm_factor
+    y_agg$x[indices] <- y_agg$x[indices] * norm_factor
+  }
+  
+  # Calculate residuals
+  resid <- y_est_agg$x - y_agg$x
+  
+  # Compute ACF and PACF
+  bacf <- acf(resid, lag.max = 10, plot = FALSE)
+  bacfdf <- with(bacf[1:10], data.frame(lag, acf))
+  ciline <- qnorm((1 - 0.95) / 2) / sqrt(length(y_agg$x))
+  
+  ACF <- ggplot(data = bacfdf, mapping = aes(x = as.integer(lag), y = acf)) +
+    geom_hline(aes(yintercept = 0)) +
+    geom_hline(aes(yintercept = ciline), linetype = 2) +
+    geom_hline(aes(yintercept = -ciline), linetype = 2) +
+    geom_segment(mapping = aes(xend = lag, yend = 0)) +
+    labs(x = "Lag", y = "", title = "ACF") +
+    theme_ipsum_ps()
+  
+  bacf <- pacf(resid, lag.max = 10, plot = FALSE)
+  bacfdf <- with(bacf, data.frame(lag, acf))
+  
+  PACF <- ggplot(data = bacfdf, mapping = aes(x = as.integer(lag), y = acf)) +
+    geom_hline(aes(yintercept = 0)) +
+    geom_hline(aes(yintercept = ciline), linetype = 2) +
+    geom_hline(aes(yintercept = -ciline), linetype = 2) +
+    geom_segment(mapping = aes(xend = lag, yend = 0)) +
+    labs(x = "Lag", y = "", title = "PACF") +
+    theme_function()
+  
+  # Combine ACF and PACF plots
+  ggarrange(ACF, PACF, ncol = 2, nrow = 1)
+}
+
+p1 <- process_demographic_group(pr3[pr3$sexe == 0 & pr3$age == 0, ], covars[covars[, 2] == 0 & covars[, 3] == 0, ], 2009, 2016, c(9, 32), "Women 15-29 years old")
+p2 <- process_demographic_group(pr3[pr3$sexe == 0 & pr3$age == 1, ], covars[covars[, 2] == 1 & covars[, 3] == 0, ], 2009, 2016, c(1, 8), "Women 30-94 years old")
+p3 <- process_demographic_group(pr3[pr3$sexe == 1 & pr3$age == 0, ], covars[covars[, 2] == 0 & covars[, 3] == 1, ], 2009, 2016, c(4, 32), "Men 15-29 years old")
+p4 <- process_demographic_group(pr3[pr3$sexe == 1 & pr3$age == 1, ], covars[covars[, 2] == 1 & covars[, 3] == 1, ], 2009, 2016, c(1, 12), "Men 30-94 years old")
+
+# compute_global_residues(max.llh, pr3, covars, q)
+
+ggarrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+
+perform_residual_analysis(max.llh, pr3, covars, q_time)
+
